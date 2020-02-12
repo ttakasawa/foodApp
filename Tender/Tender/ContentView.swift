8927b4446fb6f751aaff7036677668fe562a506e
Tomoki Takasawa <>
2020-02-12 13:11:08 -0500

    XCode proj.

---
+++ Tender/Tender/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tender
+//
+//  Created by Tomoki Takasawa on 2/12/20.
+//  Copyright © 2020 Tender. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tender/TenderTests/TenderTests.swift
@@ -0,0 +1,34 @@
+//
+//  TenderTests.swift
+//  TenderTests
+//
+//  Created by Tomoki Takasawa on 2/12/20.
+//  Copyright © 2020 Tender. All rights reserved.
+//
+
+import XCTest
+@testable import Tender
+
+class TenderTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
